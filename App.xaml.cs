b5903b9a70845bdef7d3dab61090d3f8feadb666
AngieRB <134572357+AngieRB@users.noreply.github.com>
2023-07-02 20:40:24 -0500

    Add files via upload

---
+++ App.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using ViewMVM.View;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+
+
+namespace ViewMVM
+{
+  
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NovelaView();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
+
